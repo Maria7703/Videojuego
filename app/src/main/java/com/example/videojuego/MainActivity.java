6ffe8b231e541cf2989eaafce80a505ebab9c6e5
Madnessl3 <phentaxalex@gmail.com>
2025-06-19 13:57:47 -0600

    icono de la app y oculta barras

--- app/src/main/java/com/example/videojuego/MainActivity.java
+++ app/src/main/java/com/example/videojuego/MainActivity.java
@@ -18,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+
         // Enlazar con los IDs del XML (asegúrate de que coincidan)
         btnNueva = findViewById(R.id.btnnueva); // <-- Este ID debe existir en activity_main.xml
         btnContinuar = findViewById(R.id.btncontinuar);
