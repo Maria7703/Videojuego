56e31d0679fb7cf1d3d119392808275557876f2f
Maria7703 <maria420_yaneth@hotmail.com>
2025-06-27 10:08:13 -0600

    borre una import en iden1 y ya se va al menu cuando lo despiden

--- app/src/main/java/com/example/videojuego/Identificacion2Activity.java
+++ app/src/main/java/com/example/videojuego/Identificacion2Activity.java
@@ -1,18 +1,29 @@
 package com.example.videojuego;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Identificacion2Activity extends AppCompatActivity {
     TextView nombreEntry;
+    private static final int DURACION_MOSTRAR = 3000; // 3 segundos
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.identificacion2);
+
+        // Esperar 3 segundos y volver al menú principal
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(Identificacion2Activity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK); // limpia pila de actividades
+            startActivity(intent);
+            finish(); // cierra esta actividad
+        }, DURACION_MOSTRAR);
 
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
--- app/src/main/java/com/example/videojuego/IdentificacionActivity.java
+++ app/src/main/java/com/example/videojuego/IdentificacionActivity.java
@@ -6,7 +6,6 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Handler;
 
 
 public class IdentificacionActivity extends AppCompatActivity {
